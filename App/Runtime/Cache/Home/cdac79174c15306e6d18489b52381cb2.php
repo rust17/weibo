ce851b32d86acfc517d738ec1aacabc004f30254
circle <newnash@163.com>
2016-08-17 21:32:49 +0800

    微博发布区设计

--- App/Runtime/Cache/Home/cdac79174c15306e6d18489b52381cb2.php
+++ App/Runtime/Cache/Home/cdac79174c15306e6d18489b52381cb2.php
@@ -56,11 +56,22 @@
 
 <div id="main">
     
-    main
+    <div class="main_left">
+        <div class="weibo_form">
+            <span class="left">和大家分享一点新鲜事吧？</span>
+            <span class="right weibo_num">可以输入<strong>140</strong>个字</span>
+            <textarea class="weibo_text"></textarea>
+            <input class="weibo_button" type="button" value="发布"/>
+        </div>
+    </div>
+    <div class="main_right">
+        right
+    </div>
 
 </div>
 
-<div id="footer">
+<div id="error">...</div>
+<div id="foot">
     <div class="footer_left">&copy; 2016 Ycku.com All Rights Reserved.</div>
     <div class="footer_right">Powered by ThinkPHP</div>
 </div>
--- Public/Home/js/index.js
+++ Public/Home/js/index.js
@@ -2,6 +2,7 @@
  * Created by lenovo on 2016/8/15.
  */
 $(function(){
+    //消息和账号的下拉菜单
     $('.app').hover(function(){
         $(this).css({
             background : '#fff',
@@ -13,4 +14,63 @@ $(function(){
             color: '#fff',
         }).find('.list').hide();
     });
+
+    //微博高度保持一致
+    if($('.main_left').height()>800){
+        $('.main_right').height($('.main_left').height());
+        $('#main').height($('.main_left').height());
+    }
+
+    //微博发布的按钮
+    $('.weibo_button').button().click(function(e){
+        if($('.weibo_text').val().length == 0){
+            $('#error').html('请输入微博内容...').dialog('open');
+            setTimeout(function(){
+                $('#error').html('').dialog('close');
+                $('.weibo_text').focus();
+            },1000);
+        }else{
+            if(weibo_num()) {
+                alert('提交发布');
+            }
+        }
+    });
+
+    //微博输入内容计算字个数
+    $('.weibo_text').on('keyup',weibo_num);
+
+    //140字检测
+    function weibo_num(){
+        var total = 288;
+        var len = $('.weibo_text').val().length;
+        var temp = 0;
+        if(len > 0){
+            for(var i = 0;i < len; i++){
+                if($('.weibo_text').val().charCodeAt(i) > 255){
+                    temp += 2;
+                }else{
+                    temp ++;
+                }
+            }
+            var result = parseInt((total - temp)/2);
+            if(result >= 0) {
+                $('.weibo_num').html('您还可以输入<strong>' + result + '</strong>个字');
+                return true;
+            }
+        }else{
+            $('.weibo_num').html('已经超过了<strong class="red">' + result + '</strong>个字');
+            return false;
+        }
+    }
+
+    //error
+    $('#error').dialog({
+        width:190,
+        height:40,
+        closeonEscape:false,
+        modal:true,
+        resizable:false,
+        draggable:false,
+        autoOpen:false,
+    }).parent().find('.ui-widget-header').hide();
 })
